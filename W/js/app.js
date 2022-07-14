b5e0cd0d54578f6d2a3db477502412faf1ab0334
Zach <zacbenincasa@gamil.com>
2022-07-14 16:36:15 -0600

    added warmupfiles

---
+++ W/js/app.js
@@ -0,0 +1,29 @@
+function thatlog(string){
+    console.log(string)
+}
+thatlog("hello")
+
+
+function bobo(){
+
+    if(happy = true){
+        console.log("smiles")
+    }else{
+        console.log("sadface")
+    }
+}
+bobo(happy=true)
+
+
+function math(number1,number2){
+    
+    if (number1>number2){
+        console.log(number1)
+    }else if(number2>number1){
+        console.log(number2)
+    }else{
+        console.log("wtf")
+    }
+}
+
+math(10,15)
